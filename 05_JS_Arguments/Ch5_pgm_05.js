2b090b10f885e1696a6e7eb1755ae8ae13e3cdd0
priya <priyadharshmahimai89@gmail.com>
2024-07-10 19:13:56 +0200

    completed

--- 05_JS_Arguments/Ch5_pgm_05.js
+++ 05_JS_Arguments/Ch5_pgm_05.js
@@ -13,7 +13,27 @@ square(-2);
 square(1111);
 square(0.5);
 
+var cube = function (numberToCube) {
+  var result;
+  result = numberToCube * numberToCube * numberToCube;
+  console.log(numberToCube + " * " + numberToCube + " * " + numberToCube + " = " + result);
+};
 
+cube(2);
+cube(-3);
+cube(0.5);
+cube(10);
+
+var squareRoot = function (number) {
+  var result;
+  result = Math.sqrt(number);
+  console.log("The square root of " + number + " is " + result);
+};
+
+squareRoot(9);
+squareRoot(16);
+squareRoot(0.25);
+squareRoot(100); 
 
 /* Further Adventures
  *
