0af4e8c8d851388f9e0872565eaaff6efad89f87
priya <priyadharshmahimai89@gmail.com>
2024-07-11 21:39:17 +0200

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_07.js
+++ 07_JS_Object_Arguments/Ch7_pgm_07.js
@@ -5,7 +5,19 @@ var bigPlanet = planet.toUpperCase();
 
 console.log(planet + " becomes " + bigPlanet);
 
+var getBig = function (str) {
+    return str.toUpperCase();
+};
 
+var getSmall = function (str) {
+    return str.toLowerCase();
+};
+
+console.log(getBig("Earth"));
+console.log(getBig("Mars"));  
+
+console.log(getSmall("Venus")); 
+console.log(getSmall("SATURN")); 
 
 /* Further Adventures
  *
