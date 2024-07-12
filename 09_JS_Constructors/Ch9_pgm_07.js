c9526b31ed8a3ab8ab001158a28de86723f17b10
priya <priyadharshmahimai89@gmail.com>
2024-07-12 19:45:41 +0200

    completed

--- 09_JS_Constructors/Ch9_pgm_07.js
+++ 09_JS_Constructors/Ch9_pgm_07.js
@@ -26,8 +26,24 @@ question1.addOption("Bordeaux");
 question1.addOption("F");
 question1.addOption("Paris");
 question1.addOption("Brussels");
+question1.addOption("Bonjour");
 
 question1.showQuestion();
+var question2 = new QuizQuestion("What is 2 + 2?", "4");
+question2.addOption("3");
+question2.addOption("4");
+question2.addOption("5");
+question2.addOption("22");
+question2.showQuestion();
+
+var question3 = new QuizQuestion("Which planet is known as the Red Planet?", "Mars");
+question3.addOption("Earth");
+question3.addOption("Mars");
+question3.addOption("Jupiter");
+question3.addOption("Saturn");
+question3.showQuestion();
+
+
 
 
 
