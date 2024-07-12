e1dbbb0ed4f9ea416c72156838b121501e876550
priya <priyadharshmahimai89@gmail.com>
2024-07-12 18:47:55 +0200

    completed

--- 08_JS_Arrays/Ch8_pgm_12.js
+++ 08_JS_Arrays/Ch8_pgm_12.js
@@ -23,6 +23,31 @@ var displayQuestion = function (questionAndAnswer) {
     correctAnswer : "Paris"
   };
   
+  var question2 = {
+    question: "What is the largest planet in our Solar System?",
+    answers: [
+      "Earth",
+      "Mars",
+      "Jupiter",
+      "Saturn"
+    ],
+    correctAnswer: "Jupiter"
+  };
+  
+  var question3 = {
+    question: "Which element has the chemical symbol 'O'?",
+    answers: [
+      "Gold",
+      "Oxygen",
+      "Osmium",
+      "Oxium"
+    ],
+    correctAnswer: "Oxygen"
+  };
+  var questions = [question1, question2, question3];
+
+questions.forEach(displayQuestion);
+
   displayQuestion(question1);
   
   
