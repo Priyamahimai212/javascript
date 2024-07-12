8e03536db1db506478fc2796b1b49ed6ee5064a0
priya <priyadharshmahimai89@gmail.com>
2024-07-12 18:19:04 +0200

    completed

--- 08_JS_Arrays/Ch8_pgm_10.js
+++ 08_JS_Arrays/Ch8_pgm_10.js
@@ -5,9 +5,27 @@
     console.log("Index: " + index);
     console.log("Array: " + wholeArray);
   });
+  var rectangles = [
+    { length: 5, width: 3 },
+    { length: 10, width: 7 },
+    { length: 2, width: 2 }
+  ];
   
+  function assignArea(rectangle) {
+    rectangle.area = rectangle.length * rectangle.width;
+  }
   
+  function showInfo(rectangle) {
+    console.log("Length: " + rectangle.length);
+    console.log("Width: " + rectangle.width);
+    console.log("Area: " + rectangle.area);
+  }
   
+  rectangles.forEach(assignArea);
+  rectangles.forEach(showInfo);
+  
+
+
   /* Further Adventures
    *
    * 1) Create an array of rectangle objects.
