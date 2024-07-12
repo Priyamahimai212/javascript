88c1ca275bdb95d23b6a6cbe7c071ba3e206465e
priya <priyadharshmahimai89@gmail.com>
2024-07-12 19:38:00 +0200

    completed

--- 09_JS_Constructors/Ch9_pgm_06.js
+++ 09_JS_Constructors/Ch9_pgm_06.js
@@ -1,34 +1,56 @@
-var Planet = function (name, position, type) {
+var Planet = function(name, position, type) {
     this.name = name;
     this.position = position;
     this.type = type;
     this.moons = [];
   
-    this.showPlanet = function () {
-        console.log(this.name);
-        console.log("Planet " + this.position + " - " + this.type);
-        console.log("Moons: " + this.moons.join(', ') + ".");
+    this.showPlanet = function() {
+      console.log(this.name);
+      console.log("Planet " + this.position + " - " + this.type);
+      this.showMoons();
     };
   
-    this.addMoon = function (moon) {
-        this.moons.unshift(moon);
+    this.addMoon = function(moon) {
+      this.moons.unshift(moon);
     };
-};
-
-var planet1 = new Planet("Jupiter", 5, "Gas Giant");
-planet1.addMoon("Io");
-planet1.addMoon("Europa");
-
-var planet2 = new Planet("Neptune", 8, "Ice Giant");
-planet2.addMoon("Triton");
-
-var planet3 = new Planet("Mercury", 1, "Terrestrial");
-
-[ planet1, planet2, planet3 ].forEach(function (planet) {
+  
+    this.showMoons = function() {
+      this.moons.forEach(function(moon, index) {
+        console.log("(" + index + ") " + moon);
+      });
+    };
+  
+    this.getMoon = function(index) {
+      if (index >= 0 && index < this.moons.length) {
+        return this.moons[index];
+      } else {
+        return "Invalid index";
+      }
+    };
+  };
+  
+  var planet1 = new Planet("Jupiter", 5, "Gas Giant");
+  planet1.addMoon("Io");
+  planet1.addMoon("Europa");
+  planet1.addMoon("Secunda");
+  
+  var planet2 = new Planet("Neptune", 8, "Ice Giant");
+  planet2.addMoon("Triton");
+  
+  var planet3 = new Planet("Mercury", 1, "Terrestrial");
+  
+    planet1.addMoon("Ganymede");
+  planet2.addMoon("Nereid");
+  planet3.addMoon("Charon");
+  
+  [planet1, planet2, planet3].forEach(function(planet) {
     planet.showPlanet();
-});
-
-
+  });
+  
+  console.log(planet1.getMoon(1));
+  console.log(planet2.getMoon(0));
+  console.log(planet3.getMoon(1));
+  
 
 /* Further Adventures
  *
