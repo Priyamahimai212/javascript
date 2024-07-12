9c1a4994f321cdc36a2cb16c0b2cfa393e1c167d
priya <priyadharshmahimai89@gmail.com>
2024-07-12 15:45:26 +0200

    completed

--- 08_JS_Arrays/Ch8_pgm_08.js
+++ 08_JS_Arrays/Ch8_pgm_08.js
@@ -2,12 +2,15 @@
 
 var items = [ "The Pyramids", "The Grand Canyon", "Bondi Beach" ];
 
+var showItems = function(){
 console.log("Dream destinations:");
-
+console.log("Total number of places:" + items.length)
 items.forEach(function (item) {
 	console.log(" – " + item);
 });
+}
 
+showItems();
 
 
 /* Further Adventures
